6a1e6f244daa4cfa0b95aec276461a3718cb6c5a
Gary.Yu <Gary.Yu@zipmoney.com.au>
2017-11-03 13:54:31 +1100

    init

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto('http://google.com');
+await page.screenshot({path: 'google.png'});
+
+await browser.close();
+
+})();
