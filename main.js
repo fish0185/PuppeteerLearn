d983b8d4f758c62865bb674ffc4b16f494778e1a
Gary.Yu <Gary.Yu@zipmoney.com.au>
2017-11-03 15:04:08 +1100

    chore

--- main.js
+++ main.js
@@ -3,12 +3,16 @@
 const puppeteer = require('puppeteer');
 
 (async() => {
-
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto('http://google.com');
-await page.screenshot({path: 'google.png'});
-
-await browser.close();
-
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 2500 // slow down by 250ms
+  });
+  const page = await browser.newPage();
+  await page.goto('http://localhost/test/');
+  // here console is backend console.
+  page.on('console', msg => console.log('PAGE LOG:', ...msg.args));
+  // here console is doing job on Chromium 
+  await page.evaluate(() => console.log(`url is ${location.href}`));
+  await page.screenshot({path: 'google.png'});
+  await browser.close();
 })();
